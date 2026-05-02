07fddbbe9c33cc74e3455c29bb1518bdfe3b68e2
Alejandro <exmen17180501alejan@gmail.com>
2026-05-02 20:18:10 -0300

    Se resolvió el problema Nro2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total = quantity <= 0 ? 0 : quantity * unitPrice;
+            var producto = new { Code = productCode, Description = productDescription, Quantity = quantity, Total = total };
+            return $"Resumen de Producto:\nCodigo: {producto.Code} - Descripcion:{producto.Description} - Total Producto:{producto.Total}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -8,7 +8,11 @@
             ProductHelper producto = new ProductHelper();
             string etiqueta = producto.ObtenerEtiquetaProducto(12023, "Mouse", 123.99m);
             Console.WriteLine(etiqueta);
-           
+            //Prueba de problema 2
+            Problema2 producto2 = new Problema2();
+            string resumen = producto2.CrearResumenVenta(1234, "Teclado", 23, 1234.55m);
+            Console.WriteLine(resumen);
+
         }
     }
 }
